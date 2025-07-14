d2a64fcf9a0e0d1bf1ef7785be45654079636893
Nachiket <nachiketdhamane687@gmail.com>
2025-07-14 21:06:57 +0530

    Create ArithmaticOp.java
    Implemented basic arithmetic operations in Java to perform addition, subtraction, multiplication, and division using pre initialised data members.

---
+++ ArithmaticOp.java
@@ -0,0 +1,14 @@
+class ArithmaticOp {
+  public static void main (String args[]){
+    int a=10, b=2;
+    int add = a+b;
+    int sub = a-b;
+    int mul = a*b;
+    int div = a/b;
+
+    System.out.println("Addition :" + a + "+" + b "=" + add);
+    System.out.println("Substraction :" + a + "-" + b "=" + sub);
+    System.out.println("Multiplication :" + a + "*" + b "=" + mul);
+    System.out.println("Division :" + a + "/" + b "=" + div);
+  }
+}
